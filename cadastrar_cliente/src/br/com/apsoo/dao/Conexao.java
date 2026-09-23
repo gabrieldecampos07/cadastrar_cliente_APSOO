baba0550f5cd8b33084fa3a9d68dcf1ae19abd2a
Eridan <genealerooo@gmail.com>
2026-09-23 17:07:47 -0400

    feat ClienteDao implementado
    CRUD completo e Conexão iniciada

---
+++ cadastrar_cliente/src/br/com/apsoo/dao/Conexao.java
@@ -0,0 +1,25 @@
+package br.com.apsoo.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/cadastrar_cliente";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "";
+
+    private Conexao() {
+    }
+
+    public static Connection getConexao() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+
+    public static void fechar(Connection conexao) throws SQLException {
+        if (conexao != null && !conexao.isClosed()) {
+            conexao.close();
+        }
+    }
+}
